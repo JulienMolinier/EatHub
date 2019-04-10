37b14c2ba67576fa630ecbefc5d86123bcca20eb
Bastien Jard <bast.jard@gmail.com>
2019-04-10 09:57:39 +0200

    implémentation des différents fragments du restaurant

---
+++ app/src/main/java/com/example/eathub/fragments/restaurant/RestaurantMapFragement.java
@@ -0,0 +1,23 @@
+package com.example.eathub.fragments.restaurant;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eathub.R;
+
+public class RestaurantMapFragement extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.restaurantmap, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/eathub/fragments/restaurant/RestaurantProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.eathub.fragments.restaurant;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eathub.R;
+
+public class RestaurantProfileFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.restaurantprofile, container, false);
+
+        return view;
+    }
+}
